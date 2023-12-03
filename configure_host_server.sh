a84b6bfc23fe56937903d7c34962d0b031b5c76c
Jackygfraz <142648754+Jackygfraz@users.noreply.github.com>
2023-12-02 18:56:36 -0600

    Update configure_host_server.sh

--- configure_host_server.sh
+++ configure_host_server.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # sets up the host ansible server 
 
-
 # install git to server
 sudo yum install git
 # install ansible 
-
+if command -v ansible &> /dev/null; then
+    echo "Ansible is already installed."
+else
+    # Install Ansible
+    echo "Ansible is not installed. Installing..."
+    sudo yum install epel-release
+    sudo yum install ansible
+    echo "Ansible has been installed."
+fi
 # Set the SSH key file path
 ssh_key_file="$HOME/.ssh/id_rsa"
 
