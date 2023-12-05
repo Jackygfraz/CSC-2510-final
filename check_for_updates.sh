12b831870b4ebbab613510861bcc00a987844153
Jackygfraz <142648754+Jackygfraz@users.noreply.github.com>
2023-12-04 19:20:53 -0600

    Update check_for_updates.sh

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # File Name: check_for_updates.sh
 # Author: Jackson Frazier
+# input: one directory found with pwd.
 # Purpose: This file is made to be called by cron to run a automated check on all
 # installed applications on a server
 
+dir=$1
 # check dev servers
-ansible-playbook installPackagesDev.yml
+ansible-playbook "$dir/installPackagesDev.yml"
 # check test servers
-ansible-playbook installPackagesTest.yml
+ansible-playbook "$dir/installPackagesTest.yml"
 # check prod servers
-ansible-playbook installPackagesProd.yml
+ansible-playbook "$dir/installPackagesProd.yml"
